6c68cde249458e205a5ad87019a44381af3d3d01
MTimo27 <mihaitimoficiuc@gmail.com>
2021-02-16 14:21:07 +0200

    Minor style changes

--- src/App.js
+++ src/App.js
@@ -30,7 +30,9 @@ function App() {
   return (
     <div className="coin-app">
       <div className="coin-search">
-        <h1 className="coin-text">Search a currency</h1>
+        <h1 className="coin-text">
+          Cryptocurrency tracker
+        </h1>
         <form>
           <input
             type="text"
